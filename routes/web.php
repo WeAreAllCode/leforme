3688fbdc2f58f577197489ff77cc8400e5c69f67
mable <mysecondarybox@gmail.com>
2017-09-27 00:53:25 -0400

    Add lab,vv,ph + items routes to web.php + change welcome to leforme

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,22 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('leforme');
 });
+
+Route::get('lab.index', 'LabController@index')
+Route::post('lab.flavors', 'LabController@flavors');
+Route::post('lab.labels', 'LabController@labels');
+Route::post('lab.supplies', 'LabController@supplies');
+
+Route::get('vava.index', 'VavaController@index')
+Route::post('vava.juices', 'VavaController@juices');
+Route::post('vava.products', 'VavaController@products');
+Route::post('vava.supplies', 'VavaController@supplies');
+
+Route::get('purple.index', 'PurpleController@index')
+Route::post('purple.juices', 'PurpleController@juices');
+Route::post('purple.products', 'PurpleController@products');
+Route::post('purple.supplies', 'PurpleController@supplies');
+
+Route::resource('items', 'ItemController');
